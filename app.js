96ee1bc01d8993b478c778f6509d0cd2e845d970
canziani14 <mcanziani12@gmail.com>
2022-07-19 10:57:05 -0300

    agrego path

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require ("express");
 const app = express();
+const path = require("path")
 //rutas
 const mainRoutes = require ("./routes/mainRouter");
 const userRoutes = require ("./routes/userRouter");
